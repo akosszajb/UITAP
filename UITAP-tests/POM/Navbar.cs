3fd34dcdb74699ed9b73fb095ae7da4a62771026
Akos Szajbert <kisszajbert@gmail.com>
2024-11-01 13:59:35 +0100

    feat: add Navbar page object with UI element locators and click actions

---
+++ UITAP-tests/POM/Navbar.cs
@@ -0,0 +1,47 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace UITAP_tests;
+
+public class Navbar
+{
+    private readonly IWebDriver _driver;
+    
+    private readonly WebDriverWait _wait;
+
+    private IWebElement UITAPLogo => _driver.FindElement(By.CssSelector("a.nav-link[href='/']"));
+
+    private IWebElement HomeButton => _driver.FindElement(By.CssSelector("a.nav-link"));
+    
+    private IWebElement ResourcesButton =>  _driver.FindElement(By.CssSelector("a.nav-link[href='/resources']"));
+
+    private IWebElement NavbarToggler => _driver.FindElement(By.ClassName("navbar-toggler-icon"));
+    
+    private Navbar(IWebDriver driver, WebDriverWait wait)
+    {
+        _driver = driver;
+        _wait = wait;
+    }
+
+    public void UITAPLogoClick()
+    {
+        UITAPLogo.Click();
+    }
+
+    public void HomeButtonClick()
+    {
+        HomeButton.Click();
+    }
+
+    public void ResourcesButtonClick()
+    {
+        ResourcesButton.Click();
+    }
+    
+    public void OpenNavbarWithNavbarToggler()
+    {
+        NavbarToggler.Click();
+    }
+
+}
