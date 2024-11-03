d9b99a86eca1ebe7049d553533c20fe774980432
Akos Szajbert <kisszajbert@gmail.com>
2024-11-03 14:54:37 +0100

    feat: implement AJAXDataPage with the complete AJAXDataPAgeTests class

---
+++ UITAP-tests/AJAXDataPageTests.cs
@@ -0,0 +1,138 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UITAP_tests;
+
+public class AJAXDataPageTests
+{
+    private IWebDriver _driver;
+    private WebDriverWait _wait;
+    private WebDriverWait _longerWait;
+    private MainPage _mainPage;
+    private Navbar _navbar;
+    private Footer _footer;
+    private AJAXDataPage _AjaxDataPage;
+    
+    [SetUp]
+    public void Setup()
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArguments("--disable-search-engine-choice-screen");
+        options.AddArguments("--start-maximized");
+        _driver = new ChromeDriver(options);
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+        _longerWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+        _mainPage = new MainPage(_driver, _wait);
+        _navbar = new Navbar(_driver, _wait);
+        _footer = new Footer(_driver, _wait);
+        _AjaxDataPage = new AJAXDataPage(_driver, _longerWait);
+        _mainPage.LoadMainPage();
+        _mainPage.OpenAJAXDataPage();
+    }
+    
+    [TearDown]
+    public void Teardown()
+    {
+        _driver.Quit();
+    }
+    
+    [Test]
+    public void AJAXDataPageTest0_AllTextsAreVisible()
+    {
+        var title = _wait.Until(driver => driver.FindElement(By.XPath("//h3[contains(text(),'AJAX Data')]")));
+        var description1 = _wait.Until(driver => driver.FindElement(By.XPath("//p[contains(text(),'An element may appaear on a page ')]")));
+        var scenarioList = _wait.Until(driver=> driver.FindElement(By.XPath("//h4[contains(text(),'Scenario')]")));
+        var scenarioListElement1 = _wait.Until(driver => driver.FindElement(By.XPath("//li[contains(text(),'Record the following steps. Press')]")));
+        var scenarioListElement2 = _wait.Until(driver => driver.FindElement(By.XPath("//li[contains(text(),'Then execute your test to make sure it waits')]")));
+        var playground = _wait.Until(driver => driver.FindElement(By.XPath("//h4[contains(text(),'Playground')]")));
+    
+        Assert.IsTrue(title.Displayed, "Class Attribute page is not loaded properly (Title is not visible)");
+        Assert.IsTrue(description1.Displayed, "Class Attribute page is not loaded properly (Description1 is not visible)");
+        Assert.IsTrue(scenarioList.Displayed, "Class Attribute page is not loaded properly (ScenarioList is not visible)");
+        Assert.IsTrue(scenarioListElement1.Displayed, "Class Attribute page is not loaded properly (ScenarioListElement1 is not visible)");
+        Assert.IsTrue(scenarioListElement2.Displayed, "Class Attribute page is not loaded properly (ScenarioListElement2 is not visible)");
+        Assert.IsTrue(playground.Displayed, "Class Attribute page is not loaded properly (Playground is not visible)");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest1_ClickButton()
+    {
+        _AjaxDataPage.ButtonClicker();
+        var ajaxResultElement = _longerWait.Until(driver => driver.FindElement(By.ClassName("bg-success")));
+        
+        Assert.IsTrue(ajaxResultElement.Displayed, "Green result element is not loaded in 20 sec.");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest2_NavbarTest1_UITAPLogoTest()
+    {
+        _navbar.UITAPLogoClick();
+        
+        
+        var title = _wait.Until(driver => driver.FindElement(By.Id("title")));
+        Assert.IsTrue(title.Displayed, "UI Test Automation Playground title is not displayed");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest3_NavbarTest2_HomeButtonTest()
+    {
+        _navbar.HomeButtonClick();
+        
+        var title = _wait.Until(driver => driver.FindElement(By.Id("title")));
+        Assert.IsTrue(title.Displayed, "UI Test Automation Playground title is not displayed");
+    }
+
+    [Test]
+    public void AJAXDataPageTest4_NavbarTest3_ResourcesButtonTest()
+    {
+        _navbar.ResourcesButtonClick();
+        
+        var w3SchoolsLink = _wait.Until(driver => driver.FindElement(By.CssSelector("a[href='https://www.w3schools.com']")));
+        Assert.IsTrue(w3SchoolsLink.Displayed, "w3schools.com link is not displayed");
+    }
+
+    [Test]
+    public void AJAXDataPageTest5_NavbarTest3_TogglerTestInSmallerScreen()
+    {
+        _driver.Manage().Window.Size = new System.Drawing.Size(800, 600);
+        Thread.Sleep(3000);
+        _navbar.OpenNavbarWithNavbarToggler();
+        _navbar.HomeButtonClick();
+        
+        var title = _wait.Until(driver => driver.FindElement(By.Id("title")));
+        Assert.IsTrue(title.Displayed, "UI Test Automation Playground title is not displayed");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest6_FooterTest1_GithubLink()
+    {
+        _footer.OpenGithubRepoByLink();
+        var githubRepository = _wait.Until(driver => driver.FindElement(By.CssSelector("a[href='/Inflectra/ui-test-automation-playground']")));
+        Assert.IsTrue(githubRepository.Displayed, "Github Repository is not loaded");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest7_FooterTest2_RapiseLink()
+    {
+        _footer.OpenRapisePage();
+        var rapiseTitle = _wait.Until(driver => driver.FindElement(By.XPath("//h2[contains(text(),'Rapise')]")));
+        Assert.IsTrue(rapiseTitle.Displayed, "Rapise product page is not loaded (Rapise - title is not visible)");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest8_FooterTest2_InflectraCorporationLink()
+    {
+        _footer.OpenInflectraPage();
+        var inflectraTitle = _wait.Until(driver => driver.FindElement(By.XPath("//h2[contains(text(),'Quality At Its Core:')]")));
+        Assert.IsTrue(inflectraTitle.Displayed, "Inflectra page is not loaded (Inflectra - title is not visible)");
+    }
+    
+    [Test]
+    public void AJAXDataPageTest9_FooterTest3_ApacheLicenseLink()
+    {
+        _footer.OpenApacheLicense();
+        var apacheTitle = _wait.Until(driver => driver.FindElement(By.Id("apache-license-version-20")));
+        Assert.IsTrue(apacheTitle.Displayed, "Apache license page is not loaded (Apache license - title is not visible)");
+    }
+}
